bb9e062943d9601acc5e4d22ecadc927fa905fa1
Phuc2004-w <phucnht04@gmail.com>
2024-11-05 22:58:43 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,56 +1,22 @@
 ﻿using System;
-using System.Collections.Concurrent;
-using System.Net;
-using System.Net.WebSockets;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
-public class WeatherServer
+class SyncClient
 {
-    private HttpListener httpListener;
-    private ConcurrentDictionary<string, WebSocket> clients;
-
-    public WeatherServer()
+    static void Main()
     {
-        httpListener = new HttpListener();
-        clients = new ConcurrentDictionary<string, WebSocket>();
-    }
+        TcpClient client = new TcpClient("127.0.0.1", 8080);
+        NetworkStream stream = client.GetStream();
 
-    public async Task Start()
-    {
-        httpListener.Prefixes.Add("http://localhost:8080/");
-        httpListener.Start();
-        Console.WriteLine("Server started...");
+        byte[] message = Encoding.UTF8.GetBytes("Hello from Client");
+        stream.Write(message, 0, message.Length);
 
-        while (true)
-        {
-            var context = await httpListener.GetContextAsync();
-            if (context.Request.IsWebSocketRequest)
-            {
-                var wsContext = await context.AcceptWebSocketAsync(null);
-                var clientId = context.Request.Headers["Client-ID"];
-                clients[clientId] = wsContext.WebSocket;
-                _ = Task.Run(() => HandleClient(wsContext.WebSocket, clientId));
-            }
-        }
-    }
+        byte[] buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        Console.WriteLine("Received: " + response);
 
-    private async Task HandleClient(WebSocket socket, string clientId)
-    {
-        var buffer = new byte[1024];
-        while (socket.State == WebSocketState.Open)
-        {
-            var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-            if (result.MessageType == WebSocketMessageType.Text)
-            {
-                var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
-                var weatherData = Encoding.UTF8.GetBytes(message);
-                if (clients.TryGetValue("client2", out WebSocket client2Socket))
-                {
-                    await client2Socket.SendAsync(new ArraySegment<byte>(weatherData), WebSocketMessageType.Text, true, CancellationToken.None);
-                }
-            }
-        }
+        client.Close();
     }
 }
