de4b66f708002e4ff0c2336c40856c11a16f4afc
Phuc2004-w <phucnht04@gmail.com>
2024-11-05 22:55:56 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Xml;
+using System.IO;
+using System.Globalization;
+
+namespace ungdungthoitiet
+{
+    public partial class txt : Form
+    {
+        public txt()
+        {
+            InitializeComponent();
+        }
+
+
+        // http://home.openweathermap.org/users/sign_in -- link đăng ký lấy API
+        private const string API_KEY = "5998ef4364647dd762df49de360b0d32";
+
+        private const string CurrentUrl =
+            "http://api.openweathermap.org/data/2.5/weather?" +
+            "@QUERY@=@LOC@&mode=xml&units=imperial&APPID=" + API_KEY;
+        private const string ForecastUrl =
+            "http://api.openweathermap.org/data/2.5/forecast?" +
+            "@QUERY@=@LOC@&mode=xml&units=imperial&APPID=" + API_KEY;
+
+
+        private string[] QueryCodes = { "q", "zip", "id", };
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cboQuery.Items.Add("City");
+            cboQuery.Items.Add("ZIP");
+            cboQuery.Items.Add("ID");
+
+            cboQuery.SelectedIndex = 0;
+        }
+
+        private void btnForecast_Click(object sender, EventArgs e)
+        {
+
+            string url = ForecastUrl.Replace("@LOC@", txtLocation.Text);
+            url = url.Replace("@QUERY@", QueryCodes[cboQuery.SelectedIndex]);
+
+
+            using (WebClient client = new WebClient())
+            {
+
+                try
+                {
+                    DisplayForecast(client.DownloadString(url));
+                }
+                catch (WebException ex)
+                {
+                    DisplayError(ex);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Unknown error\n" + ex.Message);
+                }
+            }
+        }
+
+        private void DisplayForecast(string xml)
+        {
+
+            XmlDocument xml_doc = new XmlDocument();
+            xml_doc.LoadXml(xml);
+
+            XmlNode loc_node = xml_doc.SelectSingleNode("weatherdata/location");
+            txtCity.Text = loc_node.SelectSingleNode("name").InnerText;
+            txtCountry.Text = loc_node.SelectSingleNode("country").InnerText;
+            XmlNode geo_node = loc_node.SelectSingleNode("location");
+            txtLat.Text = geo_node.Attributes["latitude"].Value;
+            txtLong.Text = geo_node.Attributes["longitude"].Value;
+            txtId.Text = geo_node.Attributes["geobaseid"].Value;
+
+            lvwForecast.Items.Clear();
+            char degrees = (char)176;
+
+            foreach (XmlNode time_node in xml_doc.SelectNodes("//time"))
+            {
+
+                DateTime time =
+                    DateTime.Parse(time_node.Attributes["from"].Value,
+                        null, DateTimeStyles.AssumeUniversal);
+
+
+                XmlNode temp_node = time_node.SelectSingleNode("temperature");
+                string temp = temp_node.Attributes["value"].Value;
+
+                ListViewItem item = lvwForecast.Items.Add(time.DayOfWeek.ToString());
+                item.SubItems.Add(time.ToShortTimeString());
+                item.SubItems.Add(temp + degrees);
+            }
+        }
+
+        private void DisplayError(WebException exception)
+        {
+            try
+            {
+                StreamReader reader = new StreamReader(exception.Response.GetResponseStream());
+                XmlDocument response_doc = new XmlDocument();
+                response_doc.LoadXml(reader.ReadToEnd());
+                XmlNode message_node = response_doc.SelectSingleNode("//message");
+                MessageBox.Show(message_node.InnerText);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unknown error\n" + ex.Message);
+            }
+        }
+
+        private Button btnClick;
+        private TextBox txtLocation;
+        private TextBox txtCity;
+        private TextBox txtLat;
+
+        private void InitializeComponent()
+        {
+            this.btnClick = new System.Windows.Forms.Button();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtLat = new System.Windows.Forms.TextBox();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLong = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cboQuery = new System.Windows.Forms.ComboBox();
+            this.lvwForecast = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(222, 61);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(161, 23);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "dự báo thời tiết";
+            this.btnClick.UseVisualStyleBackColor = true;
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Location = new System.Drawing.Point(143, 12);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(505, 22);
+            this.txtLocation.TabIndex = 1;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(92, 103);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(195, 22);
+            this.txtCity.TabIndex = 2;
+            // 
+            // txtLat
+            // 
+            this.txtLat.Location = new System.Drawing.Point(92, 148);
+            this.txtLat.Name = "txtLat";
+            this.txtLat.Size = new System.Drawing.Size(195, 22);
+            this.txtLat.TabIndex = 3;
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(444, 106);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(192, 22);
+            this.txtCountry.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Thành phố:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(374, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Quốc gia:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Vĩ độ:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(384, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Kinh độ:";
+            // 
+            // txtLong
+            // 
+            this.txtLong.Location = new System.Drawing.Point(444, 148);
+            this.txtLong.Name = "txtLong";
+            this.txtLong.Size = new System.Drawing.Size(192, 22);
+            this.txtLong.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(63, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "ID:";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(92, 192);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(195, 22);
+            this.txtId.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 236);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Dự báo thời tiết:";
+            // 
+            // cboQuery
+            // 
+            this.cboQuery.Location = new System.Drawing.Point(7, 12);
+            this.cboQuery.Name = "cboQuery";
+            this.cboQuery.Size = new System.Drawing.Size(130, 24);
+            this.cboQuery.TabIndex = 13;
+            // 
+            // lvwForecast
+            // 
+            this.lvwForecast.HideSelection = false;
+            this.lvwForecast.Location = new System.Drawing.Point(15, 255);
+            this.lvwForecast.Name = "lvwForecast";
+            this.lvwForecast.Size = new System.Drawing.Size(633, 185);
+            this.lvwForecast.TabIndex = 15;
+            this.lvwForecast.UseCompatibleStateImageBehavior = false;
+            // 
+            // txt
+            // 
+            this.ClientSize = new System.Drawing.Size(667, 462);
+            this.Controls.Add(this.lvwForecast);
+            this.Controls.Add(this.cboQuery);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtLong);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.txtLat);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.btnClick);
+            this.Name = "txt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private TextBox txtCountry;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox txtLong;
+        private Label label5;
+        private TextBox txtId;
+        private Label label6;
+        private ComboBox cboQuery;
+        private ListView lvwForecast;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ungdungthoitiet
+{
+    static class Program
+    {
+        /// <summary>
+        /// Điểm bắt đầu của ứng dụng.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new txt()); // Tạo và hiển thị form chính
+        }
+    }
+}
